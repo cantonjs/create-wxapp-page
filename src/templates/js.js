b752c9494a6be8029c6e176b8f7176355262a195
JC <jc@ckoo.me>
2017-05-03 15:19:42 +0800

    fix template bug

--- src/templates/js.js
+++ src/templates/js.js
@@ -1,6 +1,6 @@
 // https://mp.weixin.qq.com/debug/wxadoc/dev/framework/MINA.html
 
-export default options => `// 获取全局应用程序实例对象
+export default (options) => `// 获取全局应用程序实例对象
 // const app = getApp()
 
 // 创建页面实例对象
@@ -13,7 +13,7 @@ Page({
 	// 生命周期函数--监听页面加载
 	onLoad () {
 		// TODO: onLoad
-	}
+	},
 
 	// 页面分享设置
 	onShareAppMessage () {
@@ -23,4 +23,4 @@ Page({
 		};
 	}
 });
-`
+`;
